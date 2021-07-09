272f15d2d5fdecdaa80c53a30db0860cfc77dd61
Eric Mulhern <eric.mulhern@gmail.com>
2021-07-08 21:01:20 -0400

    template for okcupid extension

---
+++ main.js
@@ -0,0 +1,13 @@
+if (document.URL == "https://www.okcupid.com/") {
+  
+// Declare constant to store DOM element pulled from profile page
+const essayElements = document.querySelectorAll('div.qmessays-essay');
+
+console.log(essayElements);
+// div.qmessays-essay contains <p> tags
+
+// For each qm-essays-essay get paragraph child and save that paragraph's value to a new variable
+// Use helper functins to scan for particular key words and replace with 
+
+
+}
